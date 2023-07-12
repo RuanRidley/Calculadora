2059a4b7ac17afd391ed6c33389a22e07203101d
Ruan Ridley <ruanridley0@gemail.com>
2023-07-11 22:19:22 -0300

    Todo o JS da Tabuada

---
+++ tabuada_script.js
@@ -0,0 +1,19 @@
+function Tabuada(){
+    var num = window.document.getElementById('num')
+    var end = window.document.getElementById('end')
+    if(num.value.length == 0 || end.value.length == 0){
+        window.alert('[ERRO]Confira os seus dados!')
+    }else{
+    var n = Number(num.value)
+    var e = Number(end.value)
+    var x = 0
+    var r = ''
+    var res = window.document.getElementById('Resposta')
+    res.innerHTML = `A tabuada do ${n} é: <br>`
+    do{
+        r = n*x
+        res.innerHTML += `${n} x ${x} = ${r} <br>` 
+        x++
+        } while(x <= e)
+    }
+}
